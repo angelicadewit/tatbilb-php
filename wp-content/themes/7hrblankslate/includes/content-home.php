8249cade79d9235fd22dc6a2e386baa3ee9c3c06
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2018-12-05 19:53:15 -0500

    fixed so that its now uploading images

---
+++ wp-content/themes/7hrblankslate/includes/content-home.php
@@ -0,0 +1,7 @@
+<section id="home">
+    <div class="container">
+        <div class="hero-container">
+            <img src="<?php echo get_template_directory_uri()?>/images/heroimage.jpg" alt="image">
+        </div>
+    </div>
+</section>
