aa228871ed9d605b7dd97d45ea4556132e1a2be4
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2018-12-08 01:43:20 -0500

    fixed up more css

--- wp-content/themes/7hrblankslate/includes/content-books.php
+++ wp-content/themes/7hrblankslate/includes/content-books.php
@@ -1,4 +1,7 @@
-<section class="books">
+<section class="books-section">
+
+<h1>Books</h1>
+<div class="books-row">
     <?php 
         $args = array(
             "post_type" => "books",
@@ -8,13 +11,14 @@
         $query = new WP_Query($args);
         if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
         
-            
-            <h1><?php the_field('book_title') ?></h1>
+        <div class="book">
+            <h2><?php the_field('book_title') ?></h2>
 
             <img src="<?php the_field('book_image')?>" alt="">
 
-            <a href="<?php the_field('amazon_link')?>">Amazon</a>
-            <a href="<?php the_field('barnes_and_noble_link')?>">Barnes and Noble</a>
+            <p><a href="<?php the_field('amazon_link')?>">Amazon</a></p>
+            <p><a href="<?php the_field('barnes_and_noble_link')?>">Barnes and Noble</a></p>
+        </div>
             
             
             
--- wp-content/themes/7hrblankslate/includes/content-home.php
+++ wp-content/themes/7hrblankslate/includes/content-home.php
@@ -1,8 +1,7 @@
 <section id="home">
     <div class="container">
         <picture class="hero-container">
-        <source media="(min-width: 700px)" srcset="<?php echo get_template_directory_uri()?>/images/heroimage.jpg 900w"> 
-            <source media="(min-width: 900px)" srcset="<?php echo get_template_directory_uri()?>/images/heroimage.jpg"> 
+            <source media="(min-width: 700px)" srcset="<?php echo get_template_directory_uri()?>/images/heroimage.jpg">  
             <img src="<?php echo get_template_directory_uri()?>/images/heromobile.jpg" alt="image">
         </picture>
     </div>
