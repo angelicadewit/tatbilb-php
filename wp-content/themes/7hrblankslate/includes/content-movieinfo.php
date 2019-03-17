0b94b911347b93f6623b4752ed0f474f767636ee
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-03-17 01:31:08 -0400

    modified the movie info section

--- wp-content/themes/7hrblankslate/includes/content-movieinfo.php
+++ wp-content/themes/7hrblankslate/includes/content-movieinfo.php
@@ -1,14 +1,23 @@
 <section class="movie-info">
-    <p class="tag">This movie is:</p>
-    <p> <?php the_field('movie_genre')?></p>
+    <div class="movie-info-wrapper">
+        <span class="movie-title"> <?php the_field('movie_name')?></span>
 
-    <p class="tag">This movie is available in the following languages:</p>
-    <p> <?php the_field('languages')?></p>
+        <div class="data-wrapper">
+            <span class="movie year"> <?php the_field('release_year')?></span> |
+            <span class="movie rating"> <?php the_field('rating')?></span> |
+            <span class="movie runtime"> <?php the_field('runtime')?></span> |
+            <span class="movie genre"> <?php the_field('genre')?></span>
+        </div>
 
-    <p class="tag">This movie is available in the following subtitles:</p>
-    <p> <?php the_field('subtitles')?></p>
+        <div class="summary-and-cast">
+            <p class="summary"><?php the_field('summary')?></p>
+            <p class="cast"><span class="starring-text">Starring:</span> <?php the_field('cast')?></p>
+        </div>
+    </div>
 
-    <p class="tag">This movie stars:</p>
-    <p> <?php the_field('cast')?></p>
+    <div class="netflix-logo">
+        <img src="<?php the_field('netflix_logo')?>" alt="Netflix">
+    </div>
+
     
 </section>
